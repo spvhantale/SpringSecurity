74bfa67f1d82998970014e25a93d0f533c86d92c
Swapnil Vhantale <103575498+spvhantale@users.noreply.github.com>
2023-03-02 18:17:14 +0530

    without authentication

--- SpringSecuritywithoutJWTandUserDetailService/src/main/java/com/swapnil/service/MyAuthentication.java
+++ SpringSecuritywithoutJWTandUserDetailService/src/main/java/com/swapnil/service/MyAuthentication.java
@@ -1,52 +1,59 @@
 package com.swapnil.service;
 
-import java.util.Collection;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
-public class MyAuthentication implements Authentication{
+import com.swapnil.model.Customer;
+import com.swapnil.repository.CustomerDAO;
 
+@Component
+public class MyAuthentication implements AuthenticationProvider{
+
+	@Autowired
+	private CustomerDAO customerDao;
+	@Autowired
+	private PasswordEncoder pEncoder;
+	
 	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object getCredentials() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object getDetails() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object getPrincipal() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean isAuthenticated() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
+		String username=authentication.getName();
+		String password=authentication.getCredentials().toString();
 		
+		Optional<Customer> opt=customerDao.findByEmail(username);
+		
+		if(opt.isPresent()) {
+			Customer customer=opt.get();
+			if(pEncoder.matches(customer.getPassword(), password)) {
+				List<GrantedAuthority> authorites=new ArrayList<>();
+				
+				return new UsernamePasswordAuthenticationToken(username,password , authorites);
+			}else {
+				throw new BadCredentialsException("wrong password");
+			}
+		}
+		throw new BadCredentialsException("Wrong credetial");
+
 	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		// TODO Auto-generated method stub
+		return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+	}
+
+	
 
 }
